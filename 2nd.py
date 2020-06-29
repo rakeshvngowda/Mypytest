91427f7784d43870d01d3b302069376ae418ea19
Rakesh <rakeshvngowda@gmail.com>
2020-06-29 13:16:10 +0530

    this f

--- 2nd.py
+++ 2nd.py
@@ -3,10 +3,16 @@ import 1st.py
 print(1st.kg_to_lbs(70))
 
 def add(a, b):
-    return a/b 
+    return a+b 
     
 def mul(a, b):
     return a*b 
+    
+def sub(a, b):
+    return a - b 
+
+def div(a, b):
+    return a/b
     
     
     
